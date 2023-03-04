349133c89d151eaafea00f5fec953c4fd09d1126
spindlymist <ocrobin@gmail.com>
2023-03-03 17:02:42 -0800

    create project with error type

---
+++ src/error.rs
@@ -0,0 +1,19 @@
+use std::backtrace::Backtrace;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("An IO error occurred: `{source}`")]
+    Io {
+        #[from]
+        source: std::io::Error,
+        backtrace: Backtrace,
+    },
+    #[error("Utf-8 sequence was invalid.")]
+    FromUtf8 {
+        #[from]
+        source: std::string::FromUtf8Error,
+        backtrace: Backtrace,
+    },
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+#![feature(error_generic_member_access)]
+#![feature(provide_any)]
+
+pub mod error;
+pub use error::Error;
+pub use error::Result;
