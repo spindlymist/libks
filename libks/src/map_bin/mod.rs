8fe48f3070a222523ac0d588b4608b3f01ce327a
spindlymist <ocrobin@gmail.com>
2026-04-22 19:23:01 -0700

    Make Tile (de)serializable

--- libks/src/map_bin/mod.rs
+++ libks/src/map_bin/mod.rs
@@ -40,6 +40,7 @@ pub struct AssetIds {
     pub gradient: AssetId,
 }
 
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Tile(pub u8, pub u8);
 
